0405a35411cd122a96129733a5a48304e3b6a65b
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2019-12-31 14:47:17 +0530

    Budget Planner completed

--- Budget-Planner(without-angular)/app.js
+++ Budget-Planner(without-angular)/app.js
@@ -4,9 +4,31 @@ const cancelBtn = document.querySelector("#btn-clear");
 const confirmBtn = document.querySelector("#btn-add");
 const list = document.querySelector('#list');
 
+const alertcntrlr=document.querySelector('ion-alert-controller');
+
+const clear = () =>{
+    reasonInput.value='';
+    amountInput.value='';
+}
+
+cancelBtn.addEventListener('click',clear);
+
 const total = document.querySelector('#total');
 confirmBtn.addEventListener('click',()=>{
+    if(amountInput.value<=0 || amountInput.value.trim().length<=0 || reasonInput.value.trim().length<=0 )
+    {
+        alertcntrlr.create({
+            message:'Please enter all fields.',
+            header:'Invalid Inputs',
+            buttons: ['Okay' ]
+        }).then(alertElement=>{
+            alertElement.present();
+        })
+        return;
+    }
+
     list.innerHTML+=reasonInput.value+" <span float-right>"+amountInput.value+"</span><br>";
     console.log(parseInt(document.querySelector('#total').innerHTML));
     total.innerHTML=parseInt(total.innerHTML)+parseInt(amountInput.value);
+    clear();
 });
